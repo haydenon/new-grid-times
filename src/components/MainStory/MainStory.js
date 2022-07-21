3f99ab8cc1109ca5deaf352b0822f4abd2c41e8e
Hayden O'Neill <hayden.oneill9@gmail.com>
2022-07-21 12:01:10 +1200

    Exercise 2

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { QUERIES } from "../../constants";
 
-const MainStory = ({
-  id,
-  title,
-  image,
-  location,
-  abstract,
-  ...delegated
-}) => {
+const MainStory = ({ id, title, image, location, abstract, ...delegated }) => {
   return (
     <Wrapper {...delegated}>
       <a href={`/story/${id}`}>
@@ -41,9 +35,20 @@ const Heading = styled.h2`
 `;
 
 const Abstract = styled.p`
+  --line-count: 8;
+
+  @media (${QUERIES.tabletOnly}) {
+    --line-count: 16;
+  }
+
   font-size: 1rem;
   margin-bottom: 1em;
   white-space: pre-wrap;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: var(--line-count);
+  overflow: hidden;
 `;
 
 const Location = styled.span`
